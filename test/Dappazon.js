a2682eb5cd1cda83e4800f34e96407aad231651d
kellyn <kellynwong@hotmail.com>
2024-01-23 01:04:36 +0800

    Create simple tests to check everything has been set up correctly

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,31 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("Dappazon", () => {
+  let dappazon;
+  let deployer, buyer;
+  beforeEach(async () => {
+    // Setup accounts
+    [deployer, buyer] = await ethers.getSigners();
 
-})
+    // Deploy contract
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+  });
+
+  describe("Deployment", () => {
+    // like categories
+    it("sets the owner", async () => {
+      const result = await dappazon.owner();
+      expect(result).to.equal(deployer.address);
+    });
+    it("has a name", async () => {
+      const result = await dappazon.name();
+      expect(result).to.equal("Dappazon");
+    });
+  });
+});
