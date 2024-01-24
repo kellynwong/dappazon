c695f90ff88640c311c01e0db8f18ba5b3692b17
kellyn <kellynwong@hotmail.com>
2024-01-24 21:56:22 +0800

    Update section component

--- src/App.js
+++ src/App.js
@@ -19,9 +19,12 @@ function App() {
   const [electronics, setElectronics] = useState(null);
   const [toys, setToys] = useState(null);
   const [clothing, setClothing] = useState(null);
+  const [item, setItem] = useState({});
+  const [toggle, setToggle] = useState(false);
 
-  const togglePop = () => {
-    console.log("togglePop..");
+  const togglePop = (item) => {
+    setItem(item);
+    toggle ? setToggle(false) : setToggle(true);
   };
 
   const loadBlockchainData = async () => {
@@ -75,6 +78,15 @@ function App() {
           />
           <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
         </>
+      )}
+      {toggle && (
+        <Product
+          item={item}
+          provider={provider}
+          account={account}
+          dappazon={dappazon}
+          togglePop={togglePop}
+        />
       )}
     </div>
   );
